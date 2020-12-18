5083264ac41afa23d90c7ea7647a368d40445d00
daryar679 <58885122+daryar679@users.noreply.github.com>
2020-12-19 02:45:42 +0300

    smart_pointers_lab

---
+++ BST.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "BST.h"
+
+int main() {
+	BST n;
+	for (int i = 0; i < 10; ++i)
+	{
+		n.insert(i);
+	}
+	n.print();
+
+	for (int i = 8; i > 2; --i)
+	{
+		n.erase(i);
+	}
+
+	n.print();
+
+	return 0;
+}
---
+++ BST.h
@@ -0,0 +1,133 @@
+#pragma once
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class BST
+{
+	struct Node
+	{
+		int key;
+		shared_ptr<Node> left_children;
+		shared_ptr<Node> right_children;
+		weak_ptr<Node>   parent;
+	};
+	shared_ptr<Node> root;
+	
+
+public:
+	BST();
+	bool find(int value);
+	void insertNode(shared_ptr<Node> &parent, shared_ptr<Node> &currNode, int value);
+	void insert(int value);
+	shared_ptr<Node>& minValue(std::shared_ptr<Node>& node) 
+	{
+		shared_ptr<Node> currNode = node;
+		while (currNode && currNode->left_children != nullptr)
+		{
+			currNode = currNode->left_children;
+		}
+		node = currNode;
+		return node;
+	};
+	void eraseNode(shared_ptr<Node>& currNode, int value);
+	void erase(int value);
+	~BST();
+};
+
+BST::BST()
+{
+	root = nullptr;
+}
+
+bool BST::find(int value)
+{	
+	while (root)
+	{
+		if (root->key == value)
+			return true;
+		
+		else if (root->key > value)
+			root = root->left_children;
+		
+		else
+			root = root->right_children;
+	}
+
+	return false;
+}
+
+void BST::insertNode(shared_ptr<Node>& parent, shared_ptr<Node>& currNode, int value)
+{
+	if (currNode==nullptr)
+	{
+		currNode = make_shared<Node>();
+		currNode->key = value;
+		currNode->left_children = nullptr;
+		currNode->right_children = nullptr;
+		currNode->parent = parent;
+		return;
+	}
+
+	if (value > currNode->key)
+		insertNode(currNode, currNode->right_children, value);
+	else
+		insertNode(currNode, currNode->left_children, value);
+}
+
+void BST::insert(int value)
+{
+	if (root == nullptr)
+	{
+		root = make_shared<Node>();
+		root->key = value;
+		root->left_children  = nullptr;
+		root->right_children = nullptr;
+		root->parent.lock()  = nullptr;
+		return;
+	}
+	else
+	{
+		insertNode(root, root, value);
+	}
+}
+
+void BST::eraseNode(shared_ptr<Node>& currNode, int value)
+{
+	if (currNode == nullptr)
+		return;
+
+	else
+	{
+		if (value > currNode->key)
+			eraseNode(currNode->right_children, value);
+		else if(value < currNode->key)
+			eraseNode(currNode->left_children, value);
+
+		else
+		{
+			if (currNode->left_children == nullptr)
+				currNode = currNode->right_children;
+			else if(currNode->right_children == nullptr)
+				currNode = currNode->left_children;
+			else
+			{
+				shared_ptr<Node> tmp = minValue(currNode->right_children);
+				currNode->key = tmp->key;
+				currNode->parent = tmp->parent;
+			}
+		}
+	}
+
+}
+
+void BST::erase(int value)
+{
+	eraseNode(root, value);
+}
+
+
+BST::~BST()
+{
+}
