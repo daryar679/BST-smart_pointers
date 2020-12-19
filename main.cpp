9308261a060e82e6ba00e5d73b69b49beeb8e5da
daryar679 <58885122+daryar679@users.noreply.github.com>
2020-12-19 03:06:19 +0300

    Update and rename BST.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "BST.h"
+
+int main() {
+	BST bst;
+	bst.insert(1);
+	bst.insert(2);
+	bst.insert(3);
+	cout << bst.find(4) << endl;
+	cout << bst.find(3) << endl;
+	
+	bst.erase(1);
+	cout << bst.find(1) << endl;
+
+	return 0;
+}
